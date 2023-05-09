fa70407c05af53a7d4d453b33616e787845ec192
Кислый Кислюк <kislball@yahoo.com>
2023-05-09 20:10:21 +0300

    Add event emitter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common'
 import { MooncatConfigModule } from '@app/config'
 import { KyselyModule } from '@app/kysely-adapter'
 import { ConfigModule } from '@nestjs/config'
+import { EventEmitterModule } from '@nestjs/event-emitter'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [ './.mooncatrc' ],
     }),
+    EventEmitterModule.forRoot(),
     MooncatConfigModule,
     KyselyModule,
   ],
